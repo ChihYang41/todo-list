051fa0cabfeb2ed94c8ce4c78433dae875c11a61
ChihYang41 <Jigsaw0401@gmail.com>
2019-08-26 02:31:32 +0800

    Updates

---
+++ precache-manifest.a2e8e3deea53ee22e1f964ddbd82a03c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed36eec7a51669b4da75ea21df967991",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "3c21f498b9efe820a917",
+    "url": "/todo-list/static/css/main.63734b00.chunk.css"
+  },
+  {
+    "revision": "f51499f2727fd803ea87",
+    "url": "/todo-list/static/js/2.9b58a264.chunk.js"
+  },
+  {
+    "revision": "3c21f498b9efe820a917",
+    "url": "/todo-list/static/js/main.f9368abe.chunk.js"
+  },
+  {
+    "revision": "f92aacde6beb39793c09",
+    "url": "/todo-list/static/js/runtime~main.a193ca8a.js"
+  }
+]);
